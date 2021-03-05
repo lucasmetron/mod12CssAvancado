4ae4671face59242a7e1817273437e1065dc5328
lucasmetron <lucasmetron@gmail.com>
2021-03-05 14:43:40 -0300

    lista com css even e odd ou 2n-1 e 2n

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let titulos = document.querySelector("[title*=tu]")
+/*let titulos = document.querySelector("[title*=tu]")
 console.log(titulos)
+
+
+setTimeout(function(){
+    let i = ""
+    i = i + 1
+    let selecao = document.querySelectorAll("li")
+    selecao[i].setAttribute("style", "color:green;" ) 
+
+    
+    console.log(selecao)
+
+},10)*/
+
